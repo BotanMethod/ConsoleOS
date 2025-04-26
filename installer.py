b8ce31a412f99bf5be2e8b7acb7eaca6027d9a37
UntitledCatDev <167792565+BotanMethod@users.noreply.github.com>
2025-04-26 13:18:30 +0300

    Update installer.py

--- installer.py
+++ installer.py
@@ -1,6 +1,21 @@
 import os
 import shutil
 import json
+import importlib
+import sys
+import subprocess
+import time
+
+def check_dependencies():
+    dependencies = ["psutil", "getpass", 'pygame', 'shutil']
+    for dep in dependencies:
+        try:
+            __import__(dep)
+        except ImportError:
+            print(f"Установка {dep}...")
+            subprocess.run([sys.executable, "-m", "pip", "install", dep])
+
+check_dependencies()
 
 def create_directory_structure(base_path):
     dirs = [
@@ -21,14 +36,6 @@ def copy_files(src, dest):
         else:
             shutil.copy2(s, d)
 
-def create_config(config_path):
-    config = {
-        "version": "1.3.0 [F]",
-        "versionyear": "2025"
-    }
-    with open(os.path.join(config_path, "cosconfig.json"), 'w') as f:
-        json.dump(config, f, indent=4)
-
 def main():
     install_dir = input("Enter installation path (default: current dir): ") or "."
     base_path = create_directory_structure(install_dir)
@@ -38,10 +45,9 @@ def main():
     copy_files("src/Configuration", os.path.join(base_path, "Configuration"))
     shutil.copy("src/kernel.py", base_path)
     
-    # Создаем конфиг
-    create_config(os.path.join(base_path, "Configuration"))
-    
-    print(f"ConsoleOS успешно установлен в {base_path}")
+    print(f"ConsoleOS successfully installed in {base_path}")
+    print('Installer will be closed automatically in 5 seconds')
+    time.sleep(5)
 
 if __name__ == "__main__":
     main()
